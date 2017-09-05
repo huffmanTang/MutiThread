b1c85e36bf03bc56d781328e82229c81a04cb9dc
huffmanTang <tangdit@foxmail.com>
2017-09-05 17:14:21 +0800

    多个线程访问统一资源容易抢夺,所以用同步机制,给共享资源加锁

--- src/main/java/com/ajex/SinThread/CommonResThread.java
+++ src/main/java/com/ajex/SinThread/CommonResThread.java
@@ -1,4 +1,28 @@
 package com.ajex.SinThread;
 
-public class CommonResThread {
+public class CommonResThread extends Thread{
+     private  int count=6;
+
+
+    public /*synchronized*/ void run()
+    {
+            count--;
+            System.out.println(this.currentThread().getName()+"count is" + count);
+
+    }
+
+    public static void main(String[] args) {
+        CommonResThread commonRes=new CommonResThread();
+        Thread t1=new Thread(commonRes,"t1");
+        Thread t2=new Thread(commonRes,"t2");
+        Thread t3=new Thread(commonRes,"t3");
+        Thread t4=new Thread(commonRes,"t4");
+        Thread t5=new Thread(commonRes,"t5");
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+    }
 }
+

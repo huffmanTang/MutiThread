b7623cb5ec2b6829396f67d8c21758135e1dfc3e
huffmanTang <tangdit@foxmail.com>
2017-09-05 17:15:14 +0800

    多个线程访问统一资源容易抢夺,所以用同步机制,给共享资源加锁

--- src/main/java/com/ajex/SinThread/CommonResThread.java
+++ src/main/java/com/ajex/SinThread/CommonResThread.java
@@ -1,5 +1,10 @@
 package com.ajex.SinThread;
-
+/**
+ * 线程安全概念：当多个线程访问某一个类（对象或方法）时，这个对象始终都能表现出正确的行为，那么这个类（对象或方法）就是线程安全的。
+ * synchronized：可以在任意对象及方法上加锁，而加锁的这段代码称为"互斥区"或"临界区"
+ * @author alienware
+ *
+ */
 public class CommonResThread extends Thread{
      private  int count=6;
 
@@ -12,6 +17,13 @@ public class CommonResThread extends Thread{
     }
 
     public static void main(String[] args) {
+        /**
+         * 分析：当多个线程访问myThread的run方法时，以排队的方式进行处理（这里排对是按照CPU分配的先后顺序而定的），
+         * 		一个线程想要执行synchronized修饰的方法里的代码：
+         * 		1 尝试获得锁
+         * 		2 如果拿到锁，执行synchronized代码体内容；拿不到锁，这个线程就会不断的尝试获得这把锁，直到拿到为止，
+         * 		   而且是多个线程同时去竞争这把锁。（也就是会有锁竞争的问题）
+         */
         CommonResThread commonRes=new CommonResThread();
         Thread t1=new Thread(commonRes,"t1");
         Thread t2=new Thread(commonRes,"t2");
